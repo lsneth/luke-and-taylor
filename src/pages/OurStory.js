30ec7708af70c0f1351c90296954c30ee4496a88
Lucas Nethercott <luke@nethercott.com>
2023-02-03 16:53:56 -0700

    remove old our story and update registry thank you

--- src/pages/OurStory.js
+++ src/pages/OurStory.js
@@ -5,70 +5,8 @@ export default function OurStory() {
         <div className='eucalyptus-bg'>
             <div className='text'>
                 <TextImage src=''/>
-                <h1>Taylor:</h1>
+                <h1>We Met at FSY</h1>
                 <p>
-                    If someone would have told me that the first Saturday in Edmond, Oklahoma
-                    I would meet my future husband I probably would have laughed because little did I
-                    know who was going to be stepping off that bus. When I first saw you Luke I thought
-                    you were so cute, but had to "play it cool" because I couldn't let you know. I
-                    texted Kylee though and talked to Danielle about you. They were convinced we would
-                    get married after I figured out how to swoop in. Oh how grateful I am that FSY had
-                    to make so many changes because all those changes led me to you and changed my life.
-                </p>
-                <p>
-                    For the next week in Oklahoma I debated whether or not you kind of liked me too. I felt
-                    like we were just naturally drawn together. We found ourselves walking together, eating
-                    together, getting ice, throwing water, sitting together, and not to mention you drank
-                    my slushie, but used that to get a hug;) smoooth! I also remember you asking me to dance
-                    and finding an excuse to sit by me at the blazing hot baseball game. Theeen we somehow
-                    convinced Ty that you needed to come to Texas with me and Lucas. That was the BEST news.
-                    You asked me on our first date and I was so excited. We stayed up late talking and then
-                    you asked to hold my hand on our date! I was beyond excited and just loved spending every
-                    second with you.
-                </p>
-                <p>
-                    Texas was so fun! We stayed up way too late and woke up really early. We danced the
-                    dances away, did cool swing dance moves, and looked for frogs. You tried to keep me on
-                    track with my masters and, well, we know how that went haha. Not going to lie, that last
-                    night in Texas I wasn't sure what was going to happen, but I was really hoping you were going
-                    to keep liking me and was a little panicked about it to be honest. After spending every day
-                    together for those three weeks I was so sad it was coming to a close. But I saw you the next
-                    day and we walked around temple square and I loved every second of it. I couldn’t stay away so
-                    of course I had to see you before I left again… and so the long distance began!
-                </p>
-                <p>
-                    I looked forward to our video calls every single day. You have no idea how much it meant to
-                    me that you would wake up just to talk to me or plan your breaks around me. Honestly though
-                    Luke, you helped me so much while I was in Arizona. It was such a hard time, but you were
-                    there ALWAYS and let me rant about all the things. I loved our long phone calls, short phone
-                    calls, Marco Polos, ugly face contests and all. You also surprised me with a Texas A&M shirt
-                    that made my whole day. Finally, after four weeks away from you it was time to come home. I
-                    didn’t think I would see you for a few days, but little did I know you planned to surprise
-                    me at the airport. I can still see you standing behind the pole and me running full speed to
-                    give you the biggest hug ever. Four weeks was just too long, but I was also just so glad you
-                    still liked me lol.
-                </p>
-                <p>
-                    A couple days later we were talking on the phone and I made a comment and then you go “I’d
-                    break up with you… well we aren’t dating… hey Taylor, do you want to be my girlfriend?” It
-                    definitely took me a second to realize you were being serious but I was STOKED. I came to see
-                    you at FSY and we went to Kiwanis Park. We were having a great time until I realized you
-                    might kiss me and I wouldn’t look at you. Finally I asked why you were looking at me and you
-                    went for it. You told me you loved me and I replied with “Already?”... Ogh! I still feel sooo
-                    bad! But on the inside I was so giddy. We then spent as much time together as possible before
-                    Rexburg. We spent time at our families’ houses, had lots of hours on video call, rode bikes,
-                    played mini golf, went to the rodeo, did an escape room, played games, went to Peach Days, you
-                    helped in my classroom, and so much more.
-                </p>
-                <p>
-                    Then there was the day you got me to hike to a mine. Thanks for being patient with me by the way.
-                    This day and the ones leading up to it I had done lots of thinking and praying and FINALLY told
-                    you I loved you. We were in the truck on our way back and it was time. I was so nervous for some
-                    reason, but you asked the right questions and gave me the perfect moment to tell you. I mean those
-                    words with my whole soul and my love grows for you each day.
-                </p>
-                <p>
-                    
                 </p>
             </div>
         </div>
--- src/pages/Registry.js
+++ src/pages/Registry.js
@@ -9,8 +9,8 @@ export default function Registry() {
                     We are so excited as we prepare to start our lives together as husband and wife.
                     The love and support we have received from our friends and family has been truly
                     overwhelming, and we are so grateful for each and every one of you. Your presence
-                    at our wedding is the greatest gift of all, and we cannot wait to celebrate this
-                    joyous occasion with you. If you would like to contribute to our home, we have
+                    at our wedding is the greatest gift of all, and we hope you are able to celebrate this
+                    joyous occasion with us. If you would like to contribute to our home, we have
                     set up a wedding registry on Amazon. Thank you for being a part of this special time in our lives.
                     <br/>
                     <br/>
