af97a6944d32e3a7f76e31eee97feacf6c2e364f
Lucas <luke@nethercott.com>
2023-02-04 06:44:47 -0700

    our story styling

--- src/pages/OurStory.js
+++ src/pages/OurStory.js
@@ -5,8 +5,22 @@ export default function OurStory() {
         <div className='eucalyptus-bg'>
             <div className='text'>
                 <TextImage src=''/>
-                <h1>We Met at FSY</h1>
+                <h1>Our Story</h1>
                 <p>
+                    <span className='dialogue'>Luke:</span> The idea of working at FSY was
+                    so exciting to me, except for the fact that dancing is literally in the
+                    job description. I almost didn’t go until Brad Wilcox visited my roommate’s
+                    religion class and told all the students that one in five YSA that work at
+                    FSY meet their eternal companion there. That was enough to get me to brave
+                    a summer of dancing haha.
+                </p>
+                {/* <p><span className='dialogue'>Taylor:</span></p> */}
+                <p>
+                    <span className='dialogue'>Luke:</span> The idea of working at FSY was so
+                    exciting to me, except for the fact that dancing is literally in the job description.
+                    I almost didn’t go until Brad Wilcox visited my roommate’s religion class and told
+                    all the students that one in five YSA that work at FSY meet their eternal companion
+                    there. That was enough to get me to brave a summer of dancing haha.
                 </p>
             </div>
         </div>
