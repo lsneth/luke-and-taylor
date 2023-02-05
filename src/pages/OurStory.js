7655836bd44b18fa5ea18226f0f3c3254c467dc7
Lucas <luke@nethercott.com>
2023-02-04 19:56:24 -0700

    fix faulty import

--- src/pages/OurStory.js
+++ src/pages/OurStory.js
@@ -1,10 +1,7 @@
-import TextImage from '../components/TextImage'
-
 export default function OurStory() {
     return (
         <div className='eucalyptus-bg'>
             <div className='text'>
-                <TextImage src=''/>
                 <h1>Our Story</h1>
                 <p>
                     <span className='dialogue'>Luke:</span> The idea of working at FSY was
